e0e46c05639e87a14a3b85acecd3f547d89b23b3
7aj2aprilio <7aj2aprilio@gmail.com>
2024-12-12 12:37:50 +0800

    modul 16

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
 
 class EmployeeSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class EmployeeSeeder extends Seeder
      */
     public function run(): void
     {
+        Employee::factory()->count(10)->create();
         DB::table('employees')->insert([
             [
                 'firstname' => 'Purnama',
--- database/seeders/PositionSeeder.php
+++ database/seeders/PositionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Models\Position;
 
 class PositionSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class PositionSeeder extends Seeder
      */
     public function run(): void
     {
+        Position::factory()->count(5)->create();
+
         DB::table('positions')->insert([
             [
                 'code' => 'FE',
